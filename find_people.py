ff9a6a41c8f0177f6c010d137c9ee483797d60fb
unknown <dmitriypopov.1993@mail.ru>
2021-11-13 13:15:03 +0300

    Added project

---
+++ find_people.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('images/5.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = cv2.CascadeClassifier('people_body.xml')
+
+results = faces.detectMultiScale(img, scaleFactor=2, minNeighbors=1)
+
+for (x, y, w, h) in results:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+
+cv2.imshow("Result", img)
+cv2. waitKey(0)
+
